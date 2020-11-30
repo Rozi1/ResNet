7927466254f2e22e4ab212e74e211430d4954989
Rozi1 <rozi.kh2@gmail.com>
2020-11-30 13:10:40 +0500

    Initial Commit

--- ImageNet/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# Additive Power-of-Two Quantization: An Efficient Non-uniform Discretization For Neural Networks
-# Yuhang Li, Xin Dong, Wei Wang
-# International Conference on Learning Representations (ICLR), 2020.
-
-
-from .resnet import *
-from .quant_layer import *
