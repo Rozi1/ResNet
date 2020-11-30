d45cca705eeb59844ca3d607fb4896228087347a
Rozi1 <rozi.kh2@gmail.com>
2020-11-30 13:33:47 +0500

    Update resnet.py

--- FashionMNIST_Without_APoT/models/resnet.py
+++ FashionMNIST_Without_APoT/models/resnet.py
@@ -66,9 +66,9 @@ class ResNet(nn.Module):
         self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(16)
         self.relu = nn.ReLU(inplace=True)
-        self.layer1 = self._make_layer(block, 16, layers[0], float=float)
-        self.layer2 = self._make_layer(block, 32, layers[1], stride=2, float=float)
-        self.layer3 = self._make_layer(block, 64, layers[2], stride=2, float=float)
+        self.layer1 = self._make_layer(block, 16, layers[0])
+        self.layer2 = self._make_layer(block, 32, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 64, layers[2], stride=2)
         self.avgpool = nn.AvgPool2d(8, stride=1)
         self.fc = last_fc(64 * block.expansion, num_classes)
 
@@ -80,7 +80,7 @@ class ResNet(nn.Module):
                 m.weight.data.fill_(1)
                 m.bias.data.zero_()
 
-    def _make_layer(self, block, planes, blocks, stride=1, float=False):
+    def _make_layer(self, block, planes, blocks, stride=1):
         downsample = None
         if stride != 1 or self.inplanes != planes * block.expansion:
             downsample = nn.Sequential(
@@ -94,7 +94,7 @@ class ResNet(nn.Module):
         layers.append(block(self.inplanes, planes, stride, downsample))
         self.inplanes = planes * block.expansion
         for _ in range(1, blocks):
-            layers.append(block(self.inplanes, planes, float=float))
+            layers.append(block(self.inplanes, planes))
 
         return nn.Sequential(*layers)
 
