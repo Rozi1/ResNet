f7409ec73533cd66beeeffa57144bed2358d1b7b
Rozi1 <rozi.kh2@gmail.com>
2020-11-30 13:17:59 +0500

    Update main.py

--- FashionMNIST_Without_APoT/main.py
+++ FashionMNIST_Without_APoT/main.py
@@ -90,14 +90,16 @@ def main():
 
     print('=> loading MNIST data...')
     normalize = transforms.Normalize(mean=[0.491, 0.482, 0.447], std=[0.247, 0.243, 0.262])
-    train_dataset = torchvision.datasets.FashionMNIST(
+    train_dataset = torchvision.datasets.CIFAR10(
         root='./data',
         train=True,
         download=True,
         transform=transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.1307,), (0.3081,))
-    ]))
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            normalize,
+        ]))
     trainloader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=2)
 
     test_dataset = torchvision.datasets.CIFAR10(
@@ -109,7 +111,6 @@ def main():
             normalize,
         ]))
     testloader = torch.utils.data.DataLoader(test_dataset, batch_size=100, shuffle=False, num_workers=2)
-
    
     if args.evaluate:
         validate(testloader, model, criterion)
